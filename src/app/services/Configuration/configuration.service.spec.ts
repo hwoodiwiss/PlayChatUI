6860862688e3431590f874be61b14a3f0cd5aeac
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-09-05 00:30:50 +0100

    Completed functional config service tests

--- src/app/services/Configuration/configuration.service.spec.ts
+++ src/app/services/Configuration/configuration.service.spec.ts
@@ -3,18 +3,11 @@ import { ConfigurationService, CFG_KEY } from './configuration.service';
 
 describe('ConfigurationService', () => {
   let service: ConfigurationService;
-  const mockLocalStorage = {
-    getItem: jest.fn(),
-    length: 0,
-    clear: jest.fn(),
-    key: jest.fn(),
-    removeItem: jest.fn(),
-    setItem: jest.fn(),
-  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ConfigurationService);
+    global.localStorage.removeItem(CFG_KEY);
   });
 
   it('should be created', () => {
@@ -41,10 +34,65 @@ describe('ConfigurationService', () => {
     const expectedJson = JSON.stringify(expectedConfig);
     const expectedB64 = btoa(expectedJson);
 
-    Object.defineProperty(global, 'localStorage', { value: mockLocalStorage });
-    mockLocalStorage.getItem.mockReturnValue(expectedB64);
+    global.localStorage.setItem(CFG_KEY, expectedB64);
 
     const actualConfig = await service.getConfiguration();
     expect(actualConfig).toEqual(expectedConfig);
+  });
+
+  it('getConfiguration should return empty object if no value is present in localStorage', async () => {
+    const actualConfig = await service.getConfiguration();
+    expect(actualConfig).toEqual({});
+  });
+
+  it('getConfiguration should return empty object if value in localStorage is invalid base64', async () => {
+    const expectedB64 = 'asdf#][]';
+
+    global.localStorage.setItem(CFG_KEY, expectedB64);
+
+    const errorSpy = jest.spyOn(console, 'error');
+
+    const actualConfig = await service.getConfiguration();
+    expect(actualConfig).toEqual({});
+    expect(errorSpy).toBeCalled();
+  });
+
+  it('getConfiguration should return empty object if value in localStorage is invalid json', async () => {
+    const expectedB64 = btoa('Some non-json value');
+
+    global.localStorage.setItem(CFG_KEY, expectedB64);
+
+    const errorSpy = jest.spyOn(console, 'error');
+
+    const actualConfig = await service.getConfiguration();
+    expect(actualConfig).toEqual({});
+    expect(errorSpy).toBeCalled();
+  });
+
+  it('updateConfig should store config to localStorage', async () => {
+    const expectedConfig = {
+      videoDeviceId: 'VidDev',
+      audioInputId: 'AudInId',
+      audioOutputId: 'AudOutId',
+    };
+    const expectedJson = JSON.stringify(expectedConfig);
+    const expectedB64 = btoa(expectedJson);
+    (service as any).config = expectedConfig;
+    service.updateConfig();
+
+    const actualB64 = global.localStorage.getItem(CFG_KEY);
+
+    expect(actualB64).toEqual(expectedB64);
+  });
+
+  it('updateConfig should initialize config if it is falsy', async () => {
+    const expectedJson = JSON.stringify({});
+    const expectedB64 = btoa(expectedJson);
+    (service as any).config = undefined;
+    service.updateConfig();
+
+    const actualB64 = global.localStorage.getItem(CFG_KEY);
+
+    expect(actualB64).toEqual(expectedB64);
   });
 });
