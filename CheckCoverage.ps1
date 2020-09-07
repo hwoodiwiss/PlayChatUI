2ab725d0d3e7bce12f0c3b227e97e03aed80c288
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-09-07 09:34:14 +0100

    Updated script with better message Updated Powershell version

--- CheckCoverage.ps1
+++ CheckCoverage.ps1
@@ -9,8 +9,6 @@ Write-Host $coverageDoc.coverage.'line-rate';
 [float]$lineRatePercentage = $lineRate * 100;
 Write-Host "Coverage line rate: $lineRatePercentage"
 if ($lineRatePercentage -lt $coverageThreshold) {
-	Write-Error -Message "Coverage percentage too low. Required: $coverageThreshold Coverage: $lineRatePercentage";
-	return 1;
+	Write-Host -Message "##vso[task.LogIssue type=error;]Coverage percentage too low. Required: $coverageThreshold Coverage: $lineRatePercentage";
+	exit 1;
 }
-
-return;
