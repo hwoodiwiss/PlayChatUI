6fd71a1646225224531e935245c489caafff1362
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-09-08 03:22:59 +0100

    Moved to node/js script for checking coverage

---
+++ checkCoverage.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+const params = {
+  coverageReportPath: null,
+  coverageThreshold: null,
+};
+
+(async () => {
+  process.argv.reduce((cmd, arg) => {
+    if (cmd) {
+      params[cmd] = arg;
+      return;
+    }
+
+    if (arg.startsWith("--")) {
+      const sub = arg.substr("--".length);
+      if (Object.keys(params).includes(sub)) {
+        if (typeof params[sub] === "boolean") {
+          params[cmd] = true;
+          return;
+        }
+
+        return sub;
+      }
+    }
+  });
+
+  if (!params.coverageReportPath) {
+    console.log("##vso[task.LogIssue type = error;]coverageReportPath is required");
+    return 1;
+  }
+
+  if (!params.coverageThreshold) {
+    console.log("##vso[task.LogIssue type = error;]coverageThreshold is required");
+    return 1;
+  }
+
+  let coverageReportBuffer = "";
+  try {
+    coverageReportBuffer = fs.readFileSync(params.coverageReportPath, "utf-8");
+  } catch (err) {
+    console.log(`##vso[task.LogIssue type = error;]${err.message}`);
+    return 1;
+  }
+
+  try {
+    const coverageReport = await require("xml2js").parseStringPromise(coverageReportBuffer);
+    const coveragePercentage = coverageReport["coverage"]["$"]["line-rate"] * 100;
+    if (coveragePercentage < params.coverageThreshold) {
+      console.log(`##vso[task.LogIssue type = error;]Coverage percentage too low. Required: ${params.coverageThreshold} Coverage: ${coveragePercentage}`);
+      return 1;
+    }
+  } catch (err) {
+    console.log(`##vso[task.LogIssue type = error;]${err.message}`);
+    return 1;
+  }
+})();
