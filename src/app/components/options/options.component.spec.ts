f4364933b73759e1083b41ff4088beb072e345cc
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-09-12 15:02:03 +0100

    Added tests for update handlers

--- src/app/components/options/options.component.spec.ts
+++ src/app/components/options/options.component.spec.ts
@@ -7,10 +7,19 @@ import { DeviceManagerService } from '../../services/DeviceManager/deviceManager
 describe('OptionsComponent', () => {
   let component: OptionsComponent;
   let fixture: ComponentFixture<OptionsComponent>;
-  let mockDeviceManager;
+  let mockDeviceManager = {
+    updateMediaDevices: jest.fn(),
+    getVideoDeviceInfo: jest.fn(),
+    getAudioInputDeviceInfo: jest.fn(),
+    getAudioOutputDeviceInfo: jest.fn(),
+    CurrentAudioInputDevice: null,
+    CurrentAudioOutputDevice: null,
+    CurrentVideoDevice: null,
+  };
 
   beforeEach(() => {
     mockDeviceManager = {
+      updateMediaDevices: jest.fn(),
       getVideoDeviceInfo: jest.fn(),
       getAudioInputDeviceInfo: jest.fn(),
       getAudioOutputDeviceInfo: jest.fn(),
@@ -36,6 +45,19 @@ describe('OptionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngInit should update the deviceManager, then set selected device ids to current device ids', async () => {
+    mockDeviceManager.updateMediaDevices.mockImplementation(async () => {
+      mockDeviceManager.CurrentAudioInputDevice = { deviceId: 'input' };
+      mockDeviceManager.CurrentAudioOutputDevice = { deviceId: 'output' };
+      mockDeviceManager.CurrentVideoDevice = { deviceId: 'video' };
+    });
+    await component.ngOnInit();
+    expect(mockDeviceManager.updateMediaDevices).toBeCalled();
+    expect(component.selectedAudioInputId).toBe('input');
+    expect(component.selectedAudioOutputId).toBe('output');
+    expect(component.selectedVideoDeviceId).toBe('video');
   });
 
   it('getAudioInputDevices will return deviceManager getAudioInputDeviceInfo', () => {
@@ -72,5 +94,32 @@ describe('OptionsComponent', () => {
     const expectedValue = 'Test Return Value';
     mockDeviceManager.CurrentVideoDevice = expectedValue;
     expect(component.getCurrentVideoDevice()).toBe(expectedValue);
+  });
+
+  it('updateAudioInput will set CurrentAudioInputDevice to be an input device with id matching selectedAudioInputId', () => {
+    const expectedDevice = { deviceId: 'thisOne' };
+    const inputDevices = [{ deviceId: 'notThis' }, expectedDevice];
+    mockDeviceManager.getAudioInputDeviceInfo.mockReturnValue(inputDevices);
+    component.selectedAudioInputId = 'thisOne';
+    component.updateAudioInput();
+    expect(mockDeviceManager.CurrentAudioInputDevice).toBe(expectedDevice);
+  });
+
+  it('updateAudioOutput will set CurrentAudioOutputDevice to be an input device with id matching selectedAudioOutputId', () => {
+    const expectedDevice = { deviceId: 'thisOne' };
+    const outputDevices = [{ deviceId: 'notThis' }, expectedDevice];
+    mockDeviceManager.getAudioOutputDeviceInfo.mockReturnValue(outputDevices);
+    component.selectedAudioOutputId = 'thisOne';
+    component.updateAudioOutput();
+    expect(mockDeviceManager.CurrentAudioOutputDevice).toBe(expectedDevice);
+  });
+
+  it('updateVideoDevice will set CurrentVideoDevice to be an input device with id matching selectedVideoDeviceId', () => {
+    const expectedDevice = { deviceId: 'thisOne' };
+    const videoDevices = [{ deviceId: 'notThis' }, expectedDevice];
+    mockDeviceManager.getVideoDeviceInfo.mockReturnValue(videoDevices);
+    component.selectedVideoDeviceId = 'thisOne';
+    component.updateVideoDevice();
+    expect(mockDeviceManager.CurrentVideoDevice).toBe(expectedDevice);
   });
 });
