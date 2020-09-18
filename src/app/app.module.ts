8534098a4dcef64a91eb3bcd46e2b71d3eb81a61
Hugo Woodiwiss <2156707+hwoodiwiss@users.noreply.github.com>
2020-09-18 16:20:06 +0100

    Move components to separate modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { DeviceManagerService } from './services/DeviceManager/deviceManager.service';
 import { ConfigurationService } from './services/Configuration/configuration.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeetingComponent } from './components/meeting/meeting.component';
-import { OptionsComponent } from './components/options/options.component';
+import { MeetingModule } from './components/meeting/meeting.module';
+import { OptionsModule } from './components/options/options.module';
 
 @NgModule({
-  declarations: [AppComponent, MeetingComponent, OptionsComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, OptionsModule, MeetingModule],
   providers: [DeviceManagerService, ConfigurationService],
   bootstrap: [AppComponent],
 })
