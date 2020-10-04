e86a59d04c29b13cf1deea27ccad50a477bbca56
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-10-04 13:23:20 +0100

    Fix copy-paste errors in unit tests

--- src/app/components/video-player/video-player.component.spec.ts
+++ src/app/components/video-player/video-player.component.spec.ts
@@ -1,33 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MeetingComponent } from './video-player.component';
+import { VideoPlayerComponent } from './video-player.component';
 import { DeviceManagerService } from 'src/app/services/DeviceManager/deviceManager.service';
 
-const mockDeviceManager = {
-  getVideoDevices: jest.fn(),
-  getAudioInputDevices: jest.fn(),
-  getAudioOutputDevices: jest.fn(),
-  CurrentAudioInputDevice: null,
-  CurrentAudioOutputDevice: null,
-  CurrentVideoDevice: null,
-};
-
 describe('MeetingComponent', () => {
-  let component: MeetingComponent;
-  let fixture: ComponentFixture<MeetingComponent>;
+  let component: VideoPlayerComponent;
+  let fixture: ComponentFixture<VideoPlayerComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MeetingComponent],
-      providers: [
-        {
-          provide: DeviceManagerService,
-          useFactory: () => mockDeviceManager,
-        },
-      ],
+      declarations: [VideoPlayerComponent],
+      providers: [],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(MeetingComponent);
+    fixture = TestBed.createComponent(VideoPlayerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
